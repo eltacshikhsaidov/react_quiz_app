beb18d87c5b2f40440402adbd7778d935f44b511
Eltac Shixseyidov <e.shixseyidov@turanbank.az>
2022-12-29 14:37:06 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Trivia API
+
+https://opentdb.com/api.php?amount=20&category=${9 to 32}&difficulty=${ easy or medium or hard }&type=multiple
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10,
+      }}
+    >
+      Made with ♥ by{" "}
+      <a
+        href="https://www.youtube.com/eltacshikhsaidov"
+        style={{ cursor: "pointer" }}
+      >
+        Eltac Shikhsaidov
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
